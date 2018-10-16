48e27a58d6861b8b1a6410d8cc68c1614cce4903
Luyao Huang <lhuang325@gatech.edu>
2018-10-16 18:44:30 -0400

    Initial commit

---
+++ Assets/Scripts/Flip.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flip : MonoBehaviour {
+
+    public GameObject FlippableObjects;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.F))
+        {
+           
+            foreach (Transform child in FlippableObjects.transform)
+            {
+                child.transform.RotateAround(transform.position,Vector3.up, 180);
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour {
+    public GameObject Door;
+    bool picked = false;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player" && picked == false)
+        {
+            picked = true;
+            Debug.Log("HERE");
+            Door.GetComponent<Door>().keyNumber+=1;
+            Destroy(this.transform.parent.gameObject);
+        }
+    }
+}
