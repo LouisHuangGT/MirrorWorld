eb2573d91542b49c7e266fc6c1272fe9556d1bdb
Luyao Huang <lhuang325@gatech.edu>
2018-10-24 20:19:46 -0400

    Add glow effect and flipping animation

--- Assets/Scripts/Flip.cs
+++ Assets/Scripts/Flip.cs
@@ -6,20 +6,48 @@ public class Flip : MonoBehaviour {
 
     public GameObject flipAxis;
     public GameObject FlippableObjects;
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public bool rotating = false;
+    public float rotationTime = 2f;
+    public float anticipatedRotationAngle = 100f;
+    float rotationStep;
+    float timer = 0f;
+    float totalRotationAngle = 0f;
+    public bool aticipatedRotation = true;
+    // Use this for initialization
+    void Start () {
+        rotationStep = anticipatedRotationAngle / (rotationTime / Time.deltaTime);
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.F))
         {
-           
-            foreach (Transform child in FlippableObjects.transform)
+            rotating = true;
+            timer = Time.time;
+            totalRotationAngle = 0f;
+        }
+
+        if (aticipatedRotation)
+        {
+            if (rotating)
             {
-                child.transform.RotateAround(flipAxis.transform.position,Vector3.up, 180);
+                FlippableObjects.transform.RotateAround(flipAxis.transform.position, Vector3.up, rotationStep);
+                totalRotationAngle += rotationStep;
+                if (Time.time - timer > rotationTime)
+                {
+                    
+                    rotating = false;
+                    FlippableObjects.transform.RotateAround(flipAxis.transform.position, Vector3.up, 180- totalRotationAngle);
+                }
             }
+        }
+        else
+        {
+            if (rotating)
+            {
+                FlippableObjects.transform.RotateAround(flipAxis.transform.position, Vector3.up, 180);
+            }
+            rotating = false;
         }
 	}
 }
